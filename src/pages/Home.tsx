fc71a712b3d69f211019701ff14a7291538116a8
thiago henrique fragata <thiago.henrique.fragata@hotmail.com>
2021-06-22 21:32:38 -0400

    feat(app): contexto de autenticação
    desenvolvimento de contexto de autenticação com useContext

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,26 @@
+import { useContext } from "react";
 import { useHistory } from "react-router-dom";
 
-import { auth, firebase } from "../services/firebase";
+import { AuthContext } from "../App";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
-import "../styles/auth.scss";
 import { Button } from "../components/Button";
+
+import "../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
+  const {user, signInWithGoogle} = useContext(AuthContext)
 
-  // autenticação com google popup
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then((result) => {
-      console.log(result);
-      history.push("/rooms/new");
-    });
+  // execução autenticação com google popup
+  async function handleCreateRoom() {
+    if(!user){
+      await signInWithGoogle();
+    }
+    history.push("/rooms/new");
   }
 
   return (
